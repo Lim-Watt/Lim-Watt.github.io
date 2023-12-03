35414dac67e6b322655f7827447d8d2c32301fa1
Lim-Watt <wht462@outlook.com>
2023-12-03 16:47:22 +0800

    Site updated: 2023-12-03 16:47:21

--- js/addEle.js
+++ js/addEle.js
@@ -4,3 +4,10 @@ var newheadBox = document.createElement('div');
 newheadBox.classList.add('newheadBox');
 cardinfoBox.classList.add('centerBox');
 cardinfoBox.insertBefore(newheadBox,cardinfoBox.childNodes[0]);
+
+var cardinfoBox2 = document.getElementsByClassName('card-info2')[0].children[0];
+console.log(cardinfoBox2);
+var newheadBox2 = document.createElement('div');
+newheadBox2.classList.add('newheadBox2');
+cardinfoBox2.classList.add('centerBox');
+cardinfoBox2.insertBefore(newheadBox2,cardinfoBox2.childNodes[0]);
